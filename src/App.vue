552f8c8aa7ff0e756e9b0a83260f9cb13be88a9a
Giuliano1993 <giuliano.gostinfini.93@gmail.com>
2022-11-14 17:00:15 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import SearchBar from "./components/SearchBar.vue";
+</script>
+
+<template>
+  <header class="fixed top-0 left-0 w-full">
+    <div class="wrapper flex py-3 bg-orange-400 justify-between">
+        <h1 class="text-2xl ml-2 w-1/3">VueTheMenue</h1>
+        <div class="w-1/3">
+          <SearchBar class="w-full "></SearchBar>
+        </div>
+        <nav class="flex w-1/3 justify-around">
+          <RouterLink class="text-white" to="/">Home</RouterLink>
+          <RouterLink class="text-white" to="/about">About</RouterLink>
+        </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,19 @@
+<script>
+import { defineComponent } from 'vue';
+export default defineComponent({
+    components: {
+
+    },
+    data : ()=>{
+        return{
+            searchString : '',
+            results: []
+        }
+    }
+
+})
+</script>
+
+<template>
+    <input :model="searchString" type="text" class="rounded-lg h-6 bg-orange-300 focus:bg-orange-100 focus-visible:border-orange-600 text-black py-6 pl-2" placeholder="Search ingredients..."/>
+</template>
