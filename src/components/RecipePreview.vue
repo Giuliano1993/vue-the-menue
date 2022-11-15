67312bb5a482124f34fcdafbfc0ef3930d8d8706
Giuliano1993 <giuliano.gostinfini.93@gmail.com>
2022-11-15 06:31:41 +0100

    mini updates to check on git action build p 3 - check on possibly undefined object

--- src/components/RecipePreview.vue
+++ src/components/RecipePreview.vue
@@ -7,9 +7,8 @@
 <template>
     <div class="flex">
         <div class="w-10 h-10 rounded-full overflow-hidden">
-            <img :src="props.recipe.image" />
+            <img :src="props?.recipe?.image ||''" />
         </div>
-        <h3 class="text-md">{{props.recipe.title}}</h3>
-        <p>lll</p>
+        <h3 class="text-md">{{props?.recipe?.title || ''}}</h3>
     </div>
 </template>
